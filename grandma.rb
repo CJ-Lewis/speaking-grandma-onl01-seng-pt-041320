1a0aa38eb68430d80c3fa97b4f90f4a53d37363d
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-15 20:41:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,9 +12,6 @@ elseif phrase == phrase.downcase
 elseif phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
-else
-  puts "NO, NOT SINCE 1938!"
-end
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
