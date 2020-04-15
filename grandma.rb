d28905332e7b170d4bb206d05c2fb3f1869cc637
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-15 20:44:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,10 @@ def speak_to_grandma
   if I LOVE YOU GRANDMA!
     puts "I LOVE YOU TOO PUMPKIN!"
     
+    elsif "Hi Nana, how are you?"
+    puts "HUH?! SPEAK UP, SONNY!"
+    
     elsif "Hi!"
     puts "HUH?! SPEAK UP, SONNY!"
     
-    elsif
+    elsif ""
